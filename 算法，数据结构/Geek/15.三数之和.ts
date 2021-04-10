6f26a0001fe411613e0f6e8bd0ee7c2c974f8ad1
Laojar <286980036@qq.com>
2021-04-10 23:55:24 +0800

    三数之和ts版；

--- 算法，数据结构/Geek/15.三数之和.ts
+++ 算法，数据结构/Geek/15.三数之和.ts
@@ -3,12 +3,13 @@
  *
  * [15] 三数之和
  */
+
 // @lc code=start
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
-// var threeSum = function(nums) {
+// var threeSum = function(nums:number[]):number[][] {
 //   let res = [];
 //   if (nums.length < 3 || nums === null) return res;
 //   let arr = nums.sort((a, b) => a - b);
@@ -31,34 +32,32 @@
 //   }
 //   return res;
 // };
-var threeSum = function (nums) {
-    var res = [];
-    var len = nums.length;
-    if (len < 3)
-        return res;
-    nums.sort(function (a, b) { return a - b; });
-    for (var i = 0; i < len - 1; i++) {
-        if (i > 0 && nums[i - 1] === nums[i])
-            continue;
-        for (var x = i + 1, y = len - 1; x < y;) {
-            var sum = nums[i] + nums[x] + nums[x];
-            if (sum === 0) {
-                res.push([nums[i], nums[x], nums[y]]);
-                while (nums[x] === nums[x - 1])
-                    x++;
-                while (nums[y] === nums[y - 1])
-                    y--;
-                x++;
-                y--;
-            }
-            else if (sum > 0)
-                y--;
-            else if (sum < 0)
-                x++;
-        }
+
+var threeSum = (nums: number[]):number[][] => {
+  let res = [];
+  let len = nums.length;
+  if (nums.length < 3 || nums === null) return res;
+  let arr = nums.sort((a, b) => a - b);
+  for (let i = 0; i < len; i++) {
+    if (arr[i] > 0) break;
+    if (i > 0 && arr[i - 1] === arr[i]) continue;
+    for (let x = i + 1, y = len - 1; x < y;) {
+      let sum = arr[i] + arr[x] + arr[y];
+      if (sum === 0) {
+        res.push([arr[i], arr[x], arr[y]]);
+        while (x<y && arr[x] === arr[x+1]) x++;
+        while (x<y && arr[y] === arr[y-1]) y--;
+        x++; y--;
+      }
+      else if (sum < 0) x++;
+      else if (sum > 0) y--;
     }
-    return res;
-};
-console.log(threeSum([-2, 0, 3, -1, 4, 0, 3, 4, 1, 1, 1, -3, -5, 4, 0]));
+  }
+  return res;
+}
+
+
+// console.log(threeSum([-1,0,1,2,-1,-4]))
+console.log(threeSum([-2,0,3,-1,4,0,3,4,1,1,1,-3,-5,4,0]))
 // @lc code=end
-//# sourceMappingURL=15.三数之和.js.map
+
