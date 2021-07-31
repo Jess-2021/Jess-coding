84b11d71ceb32d8fcb9c83b8a87acf82ad444d61
JararvisQ <286980036@qq.com>
2021-07-31 13:28:10 +0800

    264位运算

---
+++ 算法，数据结构/Geek/268.丢失的数字.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=268 lang=javascript
+ *
+ * [268] 丢失的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  return nums.concat([...(new Array(nums.length + 1)).keys()]).reduce((acc, item) => {
+    acc ^= item
+    return acc
+  }, 0)
+};
+
+var missingNumber1 = function(nums) {
+  return nums.reduce((acc, item, index) => acc ^= item ^ index, nums.length)
+}
+
+console.log(missingNumber([9,6,4,2,3,5,7,0,1]))
+// @lc code=end
+
